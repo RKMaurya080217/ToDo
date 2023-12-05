77bd9e96eea02aab33d621071f119bd893694028
RKMaurya080217 <rajatkumarmaury@gmail.com>
2023-12-05 22:37:46 +0530

    Add SQL file

---
+++ SQLs/ToDoSQLs_Query(By MySQL Workbench).sql
@@ -0,0 +1,18 @@
+use mydocument;
+
+INSERT INTO todo (task, date, status) VALUES ('Sleep', '05/12/2023', 0);
+
+SELECT * FROM mydocument.todo;
+
+SELECT * FROM todo where status ='1';
+
+SELECT * FROM todo where status ='0';
+
+UPDATE todo SET task = 'Breakfast' WHERE task ='Eat';
+
+DELETE FROM `mydocument`.`todo` WHERE task= 'Sleep';
+
+
+
+
+
